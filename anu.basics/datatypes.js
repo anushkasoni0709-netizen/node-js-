fefd29db0be82a2208c26429a526b1c6c64f90a5
anushkasoni0709-netizen <anushkasoni0709@gmail.com>
2026-08-01 08:18:42 +0000

    Added JavaScrip variables and datatypes

---
+++ anu.basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //tis will treat all code in newer version of js
+
+// alert(3+3).  we are using node js not browser
+
+console.log(3+3);
+
+let name="ANUSHKA"
+let age = 20
+let isLoggedIn = true
+let state = null
+
+console.table([name,state,isLoggedIn,state]);
+
+console.log(typeof age);
+console.log(typeof null);
+
---
+++ anu.basics/variables.js
@@ -0,0 +1,16 @@
+const accountID = 12345
+let accountEmail = "anushkasoni0709@gmail.com"
+var accountPassword = "Heloo2345"
+accountCity = "Noida"
+let accountState;
+
+// accountId = 34 // not alloweed
+//WE can only change car let and other not nut not const keyword
+// in js let dont have any scopeproblem
+// prefer not to use var bcoz of block scope and funtion scope
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
